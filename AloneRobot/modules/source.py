c9a6e80f1f51397514c8736c39982d47f04bbc9d
ASIF MOHAMMED <153398574+ASIFXQUEEN@users.noreply.github.com>
2023-12-21 14:41:14 +0530

    Update source.py

--- AloneRobot/modules/source.py
+++ AloneRobot/modules/source.py
@@ -12,19 +12,19 @@ from AloneRobot import pbot as client
 Alone = "https://telegra.ph//file/9e8ce3092848a1bc5d9d6.jpg"
 
 
-@client.on_message(filters.command(["repo", "source"]))
+@client.on_message(filters.command(["repooo", "makichud"]))
 async def repo(client, message):
     await message.reply_photo(
         photo=Alone,
         caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
 
-**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [Aʟᴏɴᴇ](tg://user?id={OWNER_ID})
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [Asif](tg://user?id={OWNER_ID})
 **» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
 **» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
 **» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
 
-**Aʟᴏɴᴇ ✘ Rᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
+**Asif ✘ Rᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
